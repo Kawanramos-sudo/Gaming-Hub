78cb5ce1174c2e55128002e99ad92222b7202e55
unknown <kawan.r2003@gmail.com>
2025-02-15 07:10:38 -0300

    Atualizacao automatica

--- atualizar.bat
+++ atualizar.bat
@@ -1,8 +1,9 @@
 @echo off
+chcp 65001 >nul
 color 0a
 cd /d "C:\Users\Kawan\Desktop\Gaminghub v2.0\Github"
 python db_json.py
 git add .
-git commit -m "Atualização automática"
+git commit -m "Atualizacao automatica"
 git push origin main
 pause
